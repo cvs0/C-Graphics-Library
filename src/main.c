829f41df315e869b6c6d77c5de26208888302612
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-12-16 13:16:31 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -303,4 +303,27 @@ typedef struct {
 #define MAINC_CANVAS_NULL ((Mainc_Canvas) {0})
 #define MAINC_PIXEL(oc, x ,y) (oc).pixels[(y) * (oc).stride + (x)]
 
+MAINCDEF Mainc_Canvas mainc_canvas(uint32_t *pixels, size_t width, size_t height, size_t stride);
+MAINCDEF Mainc_Canvas mainc_subcanvas(Mainc_Canvas oc, int x, int y, int w, int h);
+MAINCDEF bool mainc_in_bounds(Mainc_Canvas oc, int x, int y);
+MAINCDEF void mainc_blend_color(uint32_t *c1, uint32_t c2);
+MAINCDEF void mainc_fill(Mainc_Canvas oc, uint32_t color);
+MAINCDEF void mainc_rect(Mainc_Canvas oc, int x, int y, int w, int h, uint32_t color);
+MAINCDEF void mainc_frame(Mainc_Canvas oc, int x, int y, int w, int h, size_t thiccness, uint32_t color);
+MAINCDEF void mainc_circle(Mainc_Canvas oc, int cx, int cy, int r, uint32_t color);
+MAINCDEF void mainc_ellipse(Mainc_Canvas oc, int cx, int cy, int rx, int ry, uint32_t color);
+MAINCDEF void mainc_line(Mainc_Canvas oc, int x1, int y1, int x2, int y2, uint32_t color);
+MAINCDEF bool mainc_normalize_triangle(size_t width, size_t height, int x1, int y1, int x2, int y2, int x3, int y3, int *lx, int *hx, int *ly, int *hy);
+MAINCDEF bool mainc_barycentric(int x1, int y1, int x2, int y2, int x3, int y3, int xp, int yp, int *u1, int *u2, int *det);
+MAINCDEF void mainc_triangle(Mainc_Canvas oc, int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color);
+MAINCDEF void mainc_triangle3c(Mainc_Canvas oc, int x1, int y1, int x2, int y2, int x3, int y3, uint32_t c1, uint32_t c2, uint32_t c3);
+MAINCDEF void mainc_triangle3z(Mainc_Canvas oc, int x1, int y1, int x2, int y2, int x3, int y3, float z1, float z2, float z3);
+MAINCDEF void mainc_triangle3uv(Mainc_Canvas oc, int x1, int y1, int x2, int y2, int x3, int y3, float tx1, float ty1, float tx2, float ty2, float tx3, float ty3, float z1, float z2, float z3, Mainc_Canvas texture);
+MAINCDEF void mainc_triangle3uv_bilinear(Mainc_Canvas oc, int x1, int y1, int x2, int y2, int x3, int y3, float tx1, float ty1, float tx2, float ty2, float tx3, float ty3, float z1, float z2, float z3, Mainc_Canvas texture);
+MAINCDEF void mainc_text(Mainc_Canvas oc, const char *text, int x, int y, Mainc_Font font, size_t size, uint32_t color);
+MAINCDEF void mainc_sprite_blend(Mainc_Canvas oc, int x, int y, int w, int h, Mainc_Canvas sprite);
+MAINCDEF void mainc_sprite_copy(Mainc_Canvas oc, int x, int y, int w, int h, Mainc_Canvas sprite);
+MAINCDEF void mainc_sprite_copy_bilinear(Mainc_Canvas oc, int x, int y, int w, int h, Mainc_Canvas sprite);
+MAINCDEF uint32_t mainc_pixel_bilinear(Mainc_Canvas sprite, int nx, int ny, int w, int h);
+
 #endif
